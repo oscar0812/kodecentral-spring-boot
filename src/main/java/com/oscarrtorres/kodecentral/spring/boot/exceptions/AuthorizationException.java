998f6a1da0b7ae2125340742627f3ec0fdff12b3
oscar0812 <oscar0812torres@gmail.com>
2022-05-06 11:42:08 -0500

    allow for custom exception string

--- src/main/java/com/oscarrtorres/kodecentral/spring/boot/exceptions/AuthorizationException.java
+++ src/main/java/com/oscarrtorres/kodecentral/spring/boot/exceptions/AuthorizationException.java
@@ -5,4 +5,8 @@ public class AuthorizationException extends RuntimeException {
     public AuthorizationException() {
         super("Unauthorized data access");
     }
+
+    public AuthorizationException(String msg) {
+        super(msg);
+    }
 }
